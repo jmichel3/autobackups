9469c577e3b106bfd132c7e821b780af76b5342c
jmichel3 <jon.michelson93@gmail.com>
2017-01-12 20:22:24 -0500

    added env and sh prefixes to rsync command for cronjob operation

--- sync.sh
+++ sync.sh
@@ -13,22 +13,22 @@ ORIGPATH="/Users/jon_michelson"
 DESTPATH="/backupdrive/jon"
 
 # Home network's public IP address
-HOMEIP=$(cat etc/ip.txt)
+HOMEIP=$(cat $PROJPATH/etc/ip.txt)
 
 # port options
-PORT=$(cat etc/port.txt)
+PORT=$(cat $PROJPATH/etc/port.txt)
 
 # Save old timestamp
-yes | cp etc/time.txt etc/time2.txt
+yes | cp $PROJPATH/etc/time.txt $PROJPATH/etc/time2.txt
 
 # Write new timestamp
-echo $(date "+%F") > etc/time.txt
+echo $(date "+%F") > $PROJPATH/etc/time.txt
 
 # Log file blank creation
-echo "" > logs/rsync-$(date "+%F").log
+echo "" > $PROJPATH/logs/rsync-$(date "+%F").log
 
 # Rsync command
-rsync -avzhPR --chmod=Du=rwx,Dgo=rx,Fu=rw,Fgo=r --delete --log-file=$PROJPATH/logs/rsync-$(date "+%F").log --exclude-from 'etc/exclude.txt' --link-dest=$DESTPATH/$(cat $PROJPATH/etc/time2.txt) -e 'ssh -p '"$PORT"'' $ORIGPATH/ $USER@$HOMEIP:$DESTPATH/$(date "+%F")/
+env -i sh -c "rsync -avzhPR --chmod=Du=rwx,Dgo=rx,Fu=rw,Fgo=r --delete --log-file=$PROJPATH/logs/rsync-$(date "+%F").log --exclude-from ''$PROJPATH'/etc/exclude.txt' --link-dest=$DESTPATH/$(cat $PROJPATH/etc/time2.txt) -e 'ssh -p '"$PORT"'' $ORIGPATH/ $USER@$HOMEIP:$DESTPATH/$(date "+%F")/"
 
 # Copy log to Rpi
 scp -P $PORT $PROJPATH/logs/rsync-$(cat $PROJPATH/etc/time.txt).log $USER@$HOMEIP:$DESTPATH/logs/rsync-$(cat $PROJPATH/etc/time.txt).log
